28fed2aaa14caafd7c5175ef9a37ac6de63e709b
MingyuanQin <mingyuan0715@foxmail.com>
2021-05-20 23:08:40 +0800

    agential

---
+++ app/models/agential.rb
@@ -0,0 +1,11 @@
+module Agential
+
+  def self.use_relative_model_naming?
+    true
+  end
+
+  def self.table_name_prefix
+    'agential_'
+  end
+
+end
--- lib/rails_agency.rb
+++ lib/rails_agency.rb
@@ -1,10 +1,2 @@
 require 'rails_agency/engine'
 require 'rails_agency/config'
-
-module Agential
-
-  def self.use_relative_model_naming?
-    true
-  end
-
-end
